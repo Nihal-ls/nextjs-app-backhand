5d5ec0d15b3a8005a4b5bc16955a0314d61ad968
Nihal <nihallaskar888@gmail.com>
2025-11-27 02:09:18 +0600

    changed vercel.json

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 
 async function run() {
     try {
-        // await client.connect();
+        await client.connect();
 
         const db = client.db('klyvex_db');
         const productCollection = db.collection('products');
