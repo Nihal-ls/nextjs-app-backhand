1113c3fb694dbea66ab7eb6c38eb0572b98cd3bd
Nihal <nihallaskar888@gmail.com>
2025-11-27 01:56:32 +0600

    modified

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ app.use(cors());
 
 
 const port = 5000
-// EOy5ImC8mjL8kOpu
-// nihallaskar888_db_user
+
 const uri = "mongodb+srv://nihallaskar888_db_user:EOy5ImC8mjL8kOpu@klyvex-cluster.a1rycxp.mongodb.net/?appName=klyvex-cluster";
 
 const client = new MongoClient(uri, {
@@ -26,8 +25,7 @@ app.get('/', (req, res) => {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const db = client.db('klyvex_db');
         const productCollection = db.collection('products');
@@ -65,15 +63,15 @@ async function run() {
             });
         });
 
-        await client.db("admin").command({ ping: 1 });
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
 
 run().catch(console.dir);
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
